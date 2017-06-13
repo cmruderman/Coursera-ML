c7e01aabb12c4eb4584fbff3c1e6f7771e0ebd61
cmruderman <coreymruderman@gmail.com>
2017-06-12 20:54:52 -0400

    Bug fixes in lrCostFunction

--- Exercises/Python/machine-learning-ex3/lrCostFunction.py
+++ Exercises/Python/machine-learning-ex3/lrCostFunction.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from scipy.io import loadmat
 from scipy import linalg, sparse #for linalg
 import matplotlib.pyplot as plt #for the graph
@@ -8,7 +9,8 @@ from scipy.special import expit #vectorized sigmoid function
 
 datafile = 'ex3data1.mat'
 mat = loadmat( datafile )
-y =mat['y']
+y=np.mat([[1], [0], [1], [0], [1]])
+
 
 X=np.mat([[1, .1, .6, 1.1], [1, .2,.7,1.2], [1, .3, .8, 1.3], [1,.4,.9,1.4], [1, .5, 1, 1.5]])
 theta_t=np.mat([[-2], [-1], [1], [2]])
@@ -16,25 +18,31 @@ theta_t=np.mat([[-2], [-1], [1], [2]])
 m = y.shape[0]
 
 J=0
-theta = np.asmatrix(np.zeros((theta_t.shape[0], 1))); #column of 0s
+
 
 def sigmoid(z):  
     return 1 / (1 + np.exp(-z));
 
-def gradientFunc(theta_matrix, X_matrix):
-    return expit(np.dot(X_matrix,theta_matrix))
+argument=(np.dot(theta_t.T, X.T)).T
+g = sigmoid(argument)
 
-def costFunc(theta_matrix ,X_matrix,y_matrix,mylambda = 0.):
-    m = X_matrix.shape[0]
-    gradient = gradientFunc(theta_matrix,X_matrix) 
-    term1 = np.log(gradient).dot(-y_matrix.T) 
-    term2 = np.log(1.0-gradient).dot( 1 - y_matrix.T ) 
-    left_hand = (term1 - term2) / m 
-    right_hand = theta_matrix.T.dot( theta_matrix ) * mylambda / (2*m) 
-    return left_hand + right_hand 
+def gradientFunc(theta, X, y, mylambda):
+	temp=theta
+	temp[0]=0
+	grad=(1/m)*(np.dot(X.T,g-y))+(mylambda/m)*temp
+	return grad
 
-cost=costFunc(theta, X, y);
-grad=gradientFunc(theta, X);
+def costFunc(theta , X, y, mylambda):
+    m = X.shape[0]
+    summation = sum(np.dot((-1*y).T,np.log(g))-np.dot((1-y).T, np.log(1-g)))
+    summation=np.unwrap(summation)[0][0]
+    third_term=sum(np.square(theta[1:,:]))
+    J=((1./m) * summation)+(mylambda/(2*m))*np.unwrap(third_term)[0][0]
+    #(mylambda/(2*m)) is not working
+    return J
+
+cost=costFunc(theta_t, X, y, 3);
+grad=gradientFunc(theta_t, X, y, 3);
 
 print("Cost at initial theta (zeros): " +  str(cost));
 print("Expected cost: 2.534819\n");
